457c12c6b98ff40df38009dc7475e503c080e5be
Emerson Rafael <emersona7x@hotmail.com>
2025-05-02 01:58:35 -0300

    [ADD] CRIANDO DIR UTILS

--- get_weather.py
+++ get_weather.py
@@ -1,24 +1,14 @@
+import sys
 import json
+from pathlib import Path
+
 import requests
 from loguru import logger
 
+# Adicionando a pasta pai ao sys.path
+sys.path.append(str(Path(__file__).resolve().parent))
 
-def initialize_logger(log_file="weather_logs.log", log_level="INFO"):
-    """
-    Inicializa o logger com configurações padrão.
-
-    Args:
-        log_file (str): Caminho para o arquivo de log.
-        log_level (str): Nível de log (ex: "INFO", "DEBUG", "ERROR").
-    """
-    logger.add(
-        log_file,
-        rotation="1 MB",
-        level=log_level,
-        format="{time} {level} {message}",
-    )
-    logger.info("Logger inicializado com sucesso.")
-
+from utils.logger_function import initialize_logger
 
 def get_weather_data(latitude, longitude, api_key):
     """
