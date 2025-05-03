692621881755f6596979dbc801a3999010043c93
Emerson Rafael <emersona7x@hotmail.com>
2025-05-03 20:36:08 -0300

    ADICIONANDO O SYS PATH PARA QUE O PYTEST FUNCIONE CORRETAMENTE

--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -1,6 +1,13 @@
+import sys
 import json
+from pathlib import Path
+
 import pytest
 from unittest.mock import patch, MagicMock
+
+# Adiciona o diretório raiz ao sys.path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 from lambda_function import lambda_handler
 
 @pytest.fixture
